77f052677e00d9e254bf8e817b10bdef82802695
Shawn Chumbar <schumbar@ucsc.edu>
2026-03-30 12:19:04 -0700

    Update footer links to reflect correct GitHub and LinkedIn profiles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import '../assets/styles/Footer.scss'
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "../assets/styles/Footer.scss";
 
 function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/schumbar" target="_blank" rel="noreferrer">
+          <GitHubIcon />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/schumbar/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <LinkedInIcon />
+        </a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
     </footer>
   );
 }
